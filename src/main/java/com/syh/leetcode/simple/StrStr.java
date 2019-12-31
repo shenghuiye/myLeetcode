39183493212787d2c271c3184ae372ccb169d344
shenghuiye <2359783682@qq.com>
2019-12-31 10:31:18 +0800

    commit

--- src/main/java/com/syh/leetcode/simple/StrStr.java
+++ src/main/java/com/syh/leetcode/simple/StrStr.java
@@ -6,7 +6,29 @@ package com.syh.leetcode.simple;
 输出: 2
 * */
 public class StrStr {
-    public int strStr(String haystack, String needle) {
+    public int strStr1(String haystack, String needle) {
+        int txt = haystack.length();
+        int pat = needle.length();
+        for (int i=0;i<=txt-pat;i++){
+            int j;
+            for (j=0;j<pat;j++){
+                if (needle.charAt(j)!=haystack.charAt(i+j)){
+                    break;
+                }
+            }
+            if (j==pat){
+                return i;
+            }
+
+        }
         return -1;
     }
+
+    public static void main(String[] args) {
+        StrStr strStr = new StrStr();
+        int res = strStr.strStr1("","");
+        System.out.println(res);
+    }
 }
+
+
